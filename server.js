da75d2addb6395a2d28beae9ab208cfaf09cf8e6
Saurabh S <saurabh.satralkar7@gmail.com>
2024-04-11 21:11:07 +0200

    NPM package added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const requestIp = require('request-ip');
 const app = express();
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
     const visitorIP = 
-        req.headers['cf-connecting-ip'] ||  // High priority in case Cloudflare is used
-        req.headers['x-real-ip'] ||
-        req.headers['x-forwarded-for'] ||
-        req.socket.remoteAddress || '';
+        // req.headers['cf-connecting-ip'] ||  // High priority in case Cloudflare is used
+        // req.headers['x-real-ip'] ||
+        // req.headers['x-forwarded-for'] ||
+        // req.socket.remoteAddress || '';
 
+        requestIp.getClientIp(req);
         return res.json({visitorIP})
 })
 
