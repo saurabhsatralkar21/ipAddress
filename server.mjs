403750072778cd9451c0a4ecd79ab2510fcccf76
Saurabh S <saurabh.satralkar7@gmail.com>
2024-04-11 07:18:06 +0200

    Get IP address

---
+++ server.mjs
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    const visitorIP = 
+        req.headers['cf-connecting-ip'] ||  
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress || '';
+
+        return res.json({visitorIP})
+})
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+})
